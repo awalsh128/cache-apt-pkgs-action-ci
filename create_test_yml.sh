707a23aa54ef9f88d329ddb934969122d9191c75
awalsh128 <awalsh128@gomail.com>
2022-03-26 12:44:31 -0700

    Create dev template for new branch.

--- create_test_yml.sh
+++ create_test_yml.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 cat test_template.yml | \
+  sed -e 's/\$branch\$/dev/g' -e 's/\$branch_upper\$/Dev/g' -e 's/\$event_type\$/dev_push/g' \
+  > .github/workflows/dev_test.yml
+
+cat test_template.yml | \
   sed -e 's/\$branch\$/staging/g' -e 's/\$branch_upper\$/Staging/g' -e 's/\$event_type\$/staging_push/g' \
   > .github/workflows/staging_test.yml
 
