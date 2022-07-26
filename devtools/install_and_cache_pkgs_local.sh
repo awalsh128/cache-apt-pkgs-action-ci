cfb50f5e0b4ba941069d4aa19aa08ce1362b431f
awalsh128 <awalsh128@gomail.com>
2022-07-25 20:43:27 -0700

    Add parameterized arg to local testing install script.

--- devtools/install_and_cache_pkgs_local.sh
+++ devtools/install_and_cache_pkgs_local.sh
@@ -3,5 +3,5 @@
 cache_dir="/tmp/cache-apt-pkgs-action-cache"
 rm -fr ${cache_dir}; mkdir ${cache_dir}
 
-sudo apt-get autoremove --yes xdot rolldice
-time ../cache-apt-pkgs-action/install_and_cache_pkgs.sh ${cache_dir} xdot rolldice
+sudo apt-get autoremove --yes ${@}
+time ../../cache-apt-pkgs-action/install_and_cache_pkgs.sh ${cache_dir} ${@}
