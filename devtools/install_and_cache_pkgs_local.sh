4519e79ef0de60e70b979d903d904008a6a581a2
awalsh128 <awalsh128@gomail.com>
2022-08-02 19:23:01 -0700

    Add purging to local test script startup.

--- devtools/install_and_cache_pkgs_local.sh
+++ devtools/install_and_cache_pkgs_local.sh
@@ -3,5 +3,6 @@
 cache_dir="/tmp/cache-apt-pkgs-action-cache"
 rm -fr ${cache_dir}; mkdir ${cache_dir}
 
+sudo apt-get purge --yes ${@}
 sudo apt-get autoremove --yes ${@}
 time ../../cache-apt-pkgs-action/install_and_cache_pkgs.sh ${cache_dir} ${@}
