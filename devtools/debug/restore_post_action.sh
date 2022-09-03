97cd8d478e3e34e247706c7b1784ed6e035ebcd6
Andrew Walsh <awalsh128@gmail.com>
2022-09-03 12:51:24 -0700

    update-binfmts as su

--- devtools/debug/restore_post_action.sh
+++ devtools/debug/restore_post_action.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for fmt in  alpha armeb cris hexagon hppa i386 m68k microblaze mips mipsel mipsn32 mipsn32el mips64 mips64el ppc ppc64 ppc64le riscv32 riscv64 s390x sh4 sh4eb sparc sparc32plus sparc64 x86_64 xtensa xtensaeb; do
-    update-binfmts --import qemu-$fmt
-  done
+  sudo update-binfmts --import qemu-$fmt
+done
 
 if podman run --rm -it docker.io/arm64v8/alpine:3.14 uname -m; then
   echo "Command succeeded."
