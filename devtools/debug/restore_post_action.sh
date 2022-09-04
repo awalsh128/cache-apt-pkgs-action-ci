8a8823553866f0264e51746653c59b2d76956b0a
Andrew Walsh <awalsh128@gmail.com>
2022-09-03 21:10:16 -0700

    Update restore_post_action.sh

--- devtools/debug/restore_post_action.sh
+++ devtools/debug/restore_post_action.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -x
 
+apt-get install --download-only qemu-user-static
+
 mkdir /tmp/qus-ctrl
 dpkg -e /var/cache/apt/archives/qemu-user-static_1%3a4.2-3ubuntu6.23_amd64.deb /tmp/qus-ctrl
 
