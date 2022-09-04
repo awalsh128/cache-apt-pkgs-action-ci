a02c68e551870a4cb9783bf8b787b1830f01693a
Andrew Walsh <awalsh128@gmail.com>
2022-09-03 21:19:48 -0700

    Update restore_post_action.sh

--- devtools/debug/restore_post_action.sh
+++ devtools/debug/restore_post_action.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -x
 
-apt-get install --download-only qemu-user-static
-
-mkdir /tmp/qus-ctrl
-dpkg -e /var/cache/apt/archives/qemu-user-static_1%3a4.2-3ubuntu6.23_amd64.deb /tmp/qus-ctrl
-
-sh -x /tmp/qus-ctrl/postinst configure
+update-binfmts --import qemu-arm
 
 if podman run --rm -it docker.io/arm64v8/alpine:3.14 uname -m; then
   echo "Command succeeded"
