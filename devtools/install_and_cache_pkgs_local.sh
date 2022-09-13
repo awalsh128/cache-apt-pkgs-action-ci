87a68c2e0a0e7e94b60c8e7f83271a0f03d52555
awalsh128 <awalsh128@gmail.com>
2022-09-12 19:27:44 -0700

    Use postinst flag.

--- devtools/install_and_cache_pkgs_local.sh
+++ devtools/install_and_cache_pkgs_local.sh
@@ -6,4 +6,4 @@ rm -fr ${cache_dir}; mkdir ${cache_dir}
 sudo apt-get purge --yes ${@}
 sudo apt-get autoremove --yes ${@}
 time ../../cache-apt-pkgs-action/pre_cache_action.sh ${cache_dir} 1 ${@}
-time ../../cache-apt-pkgs-action/install_and_cache_pkgs.sh ${cache_dir} ${@}
+time ../../cache-apt-pkgs-action/install_and_cache_pkgs.sh ${cache_dir} true ${@}
