63db743392c71512f0ac353615cc8a043b4862cf
awalsh128 <awalsh128@gmail.com>
2022-11-24 07:51:21 -0800

    Add overwrite of tags locally when remote tags are updated.

--- devtools/update_release_tags.sh
+++ devtools/update_release_tags.sh
@@ -4,3 +4,9 @@ set -e
 
 ./copy_tag.sh ${1} v1
 ./copy_tag.sh ${1} latest
+
+cd ../../cache-apt-pkgs-action
+for branch in dev staging master; do
+  git switch ${branch}
+  git fetch origin --tags --force
+done
